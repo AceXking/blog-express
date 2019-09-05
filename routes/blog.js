c539f295f5dbda2ea9a2644f991a14358c0ad485
AceXking <443744345@qq.com>
2019-09-05 22:11:51 +0800

    router

--- routes/blog.js
+++ routes/blog.js
@@ -7,8 +7,8 @@ const {
     updateBlog,
     delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
-
-router.get('/list', function(req, res, next) {
+const loginCheck = require('../middleware/loginCheck')
+router.get('/list', (req, res, next) => {
     const author = req.query.author || ''
     const keyword = req.query.keyword || ''
     // const listData = getList(author, keyword)
@@ -19,5 +19,42 @@ router.get('/list', function(req, res, next) {
         ) 
     })
 });
-
+router.post('/new', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = newBlog(req.body)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
+})
+router.post('/update', loginCheck, (req, res, next) => {
+    const result = updateBlog(req.body.id, req.body)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel()
+            )
+        } else {
+            res.json(
+                new ErrorModel('更新失败')
+            )
+        }
+    })
+})
+router.post('/del', loginCheck, (req, res, next) => {
+    const author = req.session.username
+    const result = delBlog(req.body.id, author)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel('删除成功')
+            )
+        } else {
+            res.json(
+                new ErrorModel('删除失败')
+            )
+        }
+    })
+})
 module.exports = router;
